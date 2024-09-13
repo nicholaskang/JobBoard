70e33ccb559bd17cf1bfa368a787662a3bceb8f5
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-12 19:51:47 -0700

    Initial commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import Hero from '../components/Hero';
+import JobListings from '../components/JobListings';
+
+const JobsPage = () => {
+  return (
+    <section className='bg-blue-50 px-4 py-6'>
+      <JobListings />
+    </section>
+  )
+}
+
+export default JobsPage
