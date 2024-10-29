94c0ef8942aa4bac1604bd15f90c3970ade01bc8
Nick Kang <nickkang257@gmail.com>
2024-10-29 13:04:35 -0700

    Add reusable button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+/**
+ * @prop label, variant, isDisabled, isLoading, onClick
+ * @returns Button
+ */
+
+const Button = ({ label, variant, isDisabled, isLoading, onClick }) => {
+  const baseStyles =
+    "px-4 py-2 font-semibold rounded transition-colors duration-300";
+  const variantStyles = {
+    primary: "bg-blue-500 text-white hover:bg-blue-600",
+    secondary: "bg-gray-500 text-white hover:bg-gray-600",
+    danger: "bg-red-500 text-white hover:bg-red-600",
+  };
+  const buttonClasses = classNames(baseStyles, variantStyles[variant], {
+    "opacity-50 cursor-not-allowed": isDisabled,
+  });
+  return (
+    <button
+      className={buttonClasses}
+      variant={variant}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(["primary", "secondary", "danger"]),
+  isDisabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  variant: "primary",
+  isDisabled: false,
+  isLoading: false,
+};
